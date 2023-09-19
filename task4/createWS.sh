fef476117c24ffa87a3b1eeef51ab2690b6afb69
salmaElzoharii <100490842+salmaElzoharii@users.noreply.github.com>
2023-09-19 23:41:25 +0300

    bash script

---
+++ task4/createWS.sh
@@ -0,0 +1,12 @@
+read -p "Enter the path of the working space: " path
+read -p "Enter the name of the working space: " name
+read -p "Enter the name of the package: " package
+mkdir -p $path/$name/src
+cd $name
+catkin_make
+source ~/$name/devel/setup.bash
+cd ~/$name/src
+catkin_create_pkg $package std_msgs rospy
+cd ~/$name
+catkin_make
+. ~/$name/devel/setup.bash
